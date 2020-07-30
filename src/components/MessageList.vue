b1899506405b457ca982b6999665083a16bfe629
ezequielaranda81 <ezequiel.aranda@gmail.com>
2020-07-30 11:04:16 -0300

    Component added.

---
+++ src/components/MessageList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul id="example-1">
+      <li 
+        v-for="(item, i) in messages" 
+        :key="`item-${i}`"
+      >
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+
+<script>
+
+import {mapState} from 'vuex'
+
+export default {
+  name: 'MessageList',
+
+  computed: {
+    ...mapState('messages', [
+      'messages'
+    ]),
+  },
+
+}
+</script>
