af06c085f19b86ed6beee1918d47bb74df10b6f7
Pablo Buitrago <pablo@briefboards.com>
2020-08-18 13:20:57 -0300

    - Not-supported domain nav guard added. - Not-supported corresponding view added. - Manifest required permissions to query tabs data added.

--- src/router/index.js
+++ src/router/index.js
@@ -12,13 +12,38 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
+  },
+  {
+    path: '/not-supported',
+    name: 'NotSupported',
+    component: () => import(/* webpackChunkName: "not-supported" */ '@/views/NotSupported.vue')
   }
 ]
+
+// redirect to NotSupported if domain is not registered.
+const requiresRegisteredDomainGuard = (to, from, next) => {
+
+  chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
+    const currentTabURL = tabs[0].url
+
+    // TODO: Validate registered domain list.
+    if (currentTabURL.indexOf('canva.com') == -1 && to.name !== 'NotSupported'){
+      next({ name: 'NotSupported' })
+    }
+    else {
+      next()
+    }
+  })
+
+}
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+
+// setting nav guards
+router.beforeEach(requiresRegisteredDomainGuard)
 
 export default router
---
+++ src/views/NotSupported.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div 
+      :class="'panel'"
+    >
+      <p>TimeWise for Chrome</p>
+      <hr>
+      <p>Domain not supported.</p>
+      <button>
+        Contact Us
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotSupported'
+}
+</script>
+
+<style lang="scss" scoped>
+.panel {
+  width: 350px;
+}
+</style>
